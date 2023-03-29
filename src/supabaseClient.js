316f00297df854f23bd28a1f2c57bde9ea300a29
zschultz00 <112969769+zschultz00@users.noreply.github.com>
2023-03-29 00:05:22 -0400

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = 'https://fyadkralszkltdwayies.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5YWRrcmFsc3prbHRkd2F5aWVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczMTgwOTUsImV4cCI6MTk5Mjg5NDA5NX0.PYVE17ZspD9WXmGfjkZdPIICSc0034QLZ06JBCI7BNg';
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
